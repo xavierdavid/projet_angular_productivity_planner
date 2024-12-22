0013366aafee1e3eb33e8455b3c4b9755ca1668b
Xavier DAVID <xav.david28@gmail.com>
2024-12-22 11:59:11 +0100

    fix(auth): update env variable importation

--- src/app/core/authentication.service.ts
+++ src/app/core/authentication.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment.dev';
+import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
 /**
