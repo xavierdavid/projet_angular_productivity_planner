05be18615933297d968bb253510f397fcc8c8e57
Xavier DAVID <35581557+xavierdavid@users.noreply.github.com>
2024-12-22 12:39:20 +0100

    Live Coding n°8 : Walking Skeleton on Firebase Auth (#2)
    * fix(build): ng serve work
    
    * fix(build): add rxjs compat and update rxjs version
    
    * feat(auth): add register method to save user on firebase server
    
    * fix(auth): update env variable importation
    
    * fix(auth): remove test for auth service

---
+++ src/app/core/authentication.service.ts
@@ -0,0 +1,36 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
+
+/**
+ * Represents the payload of the response received when registering a new user in Firebase
+ * 
+ * @see https://firebase.google.com/docs/reference/rest/auth?hl=fr#section-create-email-password
+ */
+interface FirebaseResponseRegister {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+} 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  // Injection du service HTTPClient dans la propriété de classe
+  private readonly http = inject(HttpClient);
+
+  // Requête d'inscription d'un nouvel utilisateur
+  register(email:string, password:string): Observable<FirebaseResponseRegister> {
+    // URL de requête d'inscription d'un nouvel utilisateur
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseConfig.apiKey}`
+    // Body de la requête 
+    const body = {email,password,"returnSecureToken":true};
+    // Requête POST
+    return this.http.post<FirebaseResponseRegister>(url,body);
+  }
+}
