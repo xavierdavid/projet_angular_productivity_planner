4e959ea6e9af0c6e29aa56ddb02e8e72f0894c05
Xavier DAVID <xav.david28@gmail.com>
2024-12-26 16:25:38 +0100

    feat(auth): add login and save methods to write user data on firebase server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthenticationService } from './core/authentication.service';
+import { switchMap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -9,4 +12,23 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'productivity-planner';
+
+  // Gestion du bouton déclenchant l'authentification d'un utilisateur inscrit
+  private readonly authenticationService = inject(AuthenticationService)
+
+  onLogin() {
+    const email = 'john.doe@gmail.com';
+    const password = 'azerty';
+    this.authenticationService
+    .login(email, password)
+    // Gestion des observables en chaîne retournés par le service d'authentification
+    .pipe(switchMap((response) => {
+      console.log(response);
+      // Destructuration des propriétés de la réponse dont nous avons besoin
+      const {email, localId, idToken} = response;
+      return this.authenticationService.save(email, localId, idToken);
+    }))
+    // Abonnement à l'observable de sauvegarde du service d'authentification
+    .subscribe((response) => console.log(response));
+  }
 }
