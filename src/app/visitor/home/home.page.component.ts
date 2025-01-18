dc9d4ce25055353697f57bd538d253ce23610fcd
Xavier DAVID <xav.david28@gmail.com>
2025-01-18 18:51:21 +0100

    feat(home): clean legacy title and consume dumb component

--- src/app/visitor/home/home.page.component.ts
+++ src/app/visitor/home/home.page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { HomeBannerDumbComponent } from './home-banner/home-banner.dumb.component';
 
 @Component({
-  imports: [],
+  imports: [HomeBannerDumbComponent],
   templateUrl: './home.page.component.html',
   styleUrl: './home.page.component.scss'
 })
 export class HomePageComponent {
-
+  onBannerClicked() {
+    console.log('Banner clicked');
+  }
 }
