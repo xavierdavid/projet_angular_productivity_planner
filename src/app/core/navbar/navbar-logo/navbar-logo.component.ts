d256b01bb992169bf610268b0a0a0c81a525cd90
Xavier DAVID <xav.david28@gmail.com>
2025-01-27 18:26:16 +0100

    feat(navbar): generate svg component for webapp logo

---
+++ src/app/core/navbar/navbar-logo/navbar-logo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-logo',
+  standalone: true,
+  templateUrl: './navbar-logo.component.svg',
+})
+export class NavbarLogoComponent {}
