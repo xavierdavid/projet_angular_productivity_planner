06b13cb8706c02b932aea349723de13be75e12f2
Xavier DAVID <xav.david28@gmail.com>
2025-01-27 18:35:04 +0100

    test(navbar): should display title

--- src/app/core/navbar/navbar.smart.component.spec.ts
+++ src/app/core/navbar/navbar.smart.component.spec.ts
@@ -1,23 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarSmartComponent } from './navbar.smart.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('NavbarSmartComponent', () => {
   let component: NavbarSmartComponent;
   let fixture: ComponentFixture<NavbarSmartComponent>;
+  let title: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NavbarSmartComponent]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(NavbarSmartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    title = fixture.debugElement.query(By.css('[data-testid="navbar-title"]'));
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  
+  it('should display webapp title', () => {
+    expect(title.nativeElement.textContent).toContain('Productivity Planner');
   });
 });
