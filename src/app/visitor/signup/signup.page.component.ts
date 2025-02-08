c070e4d0a877a43284c4ea62a16b41c1c1fd2a48
Xavier DAVID <xav.david28@gmail.com>
2025-02-08 11:33:50 +0100

    wip(signup): generate page boilerplate

---
+++ src/app/visitor/signup/signup.page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './signup.page.component.html',
+  styleUrl: './signup.page.component.scss'
+})
+export class SignupPageComponent {
+
+}
