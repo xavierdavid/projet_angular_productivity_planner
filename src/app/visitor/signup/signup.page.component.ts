9ef8bf1d584e0ff168344d5c560b0365b7227c7d
Xavier DAVID <xav.david28@gmail.com>
2025-02-08 14:19:53 +0100

    wip(signup): setup initial base

--- src/app/visitor/signup/signup.page.component.ts
+++ src/app/visitor/signup/signup.page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.page.component.html',
-  styleUrl: './signup.page.component.scss'
+  styleUrl: './signup.page.component.scss',
 })
 export class SignupPageComponent {
+  readonly name = signal('');
+  readonly email = signal('');
+  readonly password = signal('');
+  readonly confirmPassword = signal('');
 
+  readonly isPasswordMatchValid = computed(
+    () => this.password === this.confirmPassword
+  );
+
+  onSubmit() {
+    console.log('Form submitted');
+  }
 }
