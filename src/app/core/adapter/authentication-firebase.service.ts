a6b1485ad8e993e210301c930b903a1013c3b377
Xavier DAVID <xav.david28@gmail.com>
2025-03-29 00:47:24 +0100

    refacto(webapp): custom path @env

--- src/app/core/adapter/authentication-firebase.service.ts
+++ src/app/core/adapter/authentication-firebase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import { environment } from '@env/environment';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs';
 
