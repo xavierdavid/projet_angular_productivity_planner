e9b7d3a36ccfa45eff20fb5947091cf0237b0ed9
Xavier DAVID <xav.david28@gmail.com>
2025-03-28 22:26:21 +0100

    test(signup): email field errors

--- src/app/visitor/signup/signup.page.component.spec.ts
+++ src/app/visitor/signup/signup.page.component.spec.ts
@@ -101,25 +101,104 @@ describe('SignupPageComponent', () => {
   });
 
   describe('when user interact with email field', () => {
-    it.todo('should display error message when field is empty');
-    it.todo('should display error message when field do not contain a valid HTML5 email');
+    it('should display error message when field is empty', () => {
+      email.nativeElement.value = '';
+      email.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-email-required"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Email is required.');
+    });
+
+    it('should display error message when field do not contain a valid HTML5 email', () => {
+      email.nativeElement.value = 'invalid-email';
+      email.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-email-pattern"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Email must be valid.');
+    }); 
   });
 
   describe('when user interact with password field', () => {
-    it.todo('should display error message when field is empty');
-    it.todo('should display error message when field contain less than 8 characters');
-    it.todo('should display error message when field do not contain at least 1 uppercase character');
-    it.todo('should display error message when field do not contain at least 1 lowercase character');
-    it.todo('should display error message when field do not contain at least 1 digit');
-    it.todo('should display error message when field do not contain at least 1 of the following special characters : @$!%*?&');
+    it('should display error message when field is empty', () => {
+      password.nativeElement.value = '';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-required"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password is required.');
+    });
+
+    it('should display error message when field contain less than 8 characters', () => {
+      password.nativeElement.value = 'Ab1$e';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-minlength"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password must contain at least 8 characters.');
+    });
+
+    it('should display error message when field do not contain at least 1 uppercase character', () => {
+      password.nativeElement.value = 'abc1$def';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-uppercase"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password must contain at least one uppercase letter.');
+    });
+
+    it('should display error message when field do not contain at least 1 lowercase character', () => {
+      password.nativeElement.value = 'ABC1$DEF';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-lowercase"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password must contain at least one lowercase letter.');
+    });
+
+    it('should display error message when field do not contain at least 1 digit', () => {
+      password.nativeElement.value = 'Abc$defg';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-digit"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password must contain at least one digit.');
+    });
+
+    it('should display error message when field do not contain at least 1 of the following special characters : @$!%*?&', () => {
+      password.nativeElement.value = 'Abc1defg';
+      password.nativeElement.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+
+      const error = fixture.debugElement.query(By.css('[data-testid="error-password-special"]'));
+      const errorMessage = error.nativeElement.textContent;
+
+      expect(errorMessage).toContain('Password must contain at least one special character (@$!%*?&).');
+    });
   });
 
   describe('when user interact with "confirm password" field', () => {
     it.todo('should display error message when field is empty');
     it.todo('should display error message when field do not have same value as password field');
+    });
   });
 
   describe('when user submit the form', () => {
     it.todo('should register the user');
   });
-});
+
