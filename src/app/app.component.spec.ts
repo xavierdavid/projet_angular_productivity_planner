29e29bdab573d6d05748ec92efc18d0a99d4abe6
Xavier DAVID <xav.david28@gmail.com>
2025-03-28 22:51:53 +0100

    fix(ci): repair should create of navbar and app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
  import { TestBed } from '@angular/core/testing';
  import { AppComponent } from './app.component';
+ import { provideRouter, RouterOutlet } from '@angular/router';
+ import { NavbarSmartComponent } from './core/component/navbar/navbar.smart.component';
 
  describe('AppComponent', () => {
    beforeEach(async () => {
      await TestBed.configureTestingModule({
-       imports: [AppComponent],
+      imports: [AppComponent, RouterOutlet, NavbarSmartComponent],
+      providers: [provideRouter([])]
      }).compileComponents();
    });
 
