58059d02f1ddfe2b78331682ace74866b939315c
Xavier DAVID <xav.david28@gmail.com>
2025-03-31 13:52:57 +0200

    task(dashboard): create page component dashboard with route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomePageComponent } from './visitor/home/home.page.component';
 import { SignupPageComponent } from './visitor/signup/signup.page.component';
 import { LoginPageComponent } from './visitor/login/login.page.component';
+import { DashboardPageComponent } from './membership/dashboard/dashboard.page.component';
 
 export const routes: Routes = [
   {
@@ -18,5 +19,10 @@ export const routes: Routes = [
     path: 'login',
     component: LoginPageComponent,
     title: 'Login'
+  },
+  {
+    path: 'app/dashboard',
+    component: DashboardPageComponent,
+    title: 'Dashboard'
   }
 ];
---
+++ src/app/membership/dashboard/dashboard.page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './dashboard.page.component.html',
+  styleUrl: './dashboard.page.component.scss'
+})
+export class DashboardPageComponent {
+
+}
