d8eff97cd62bdb4495d37ee05b22b25c979bc61b
Xavier DAVID <xav.david28@gmail.com>
2025-04-19 13:48:47 +0200

    wip(login): add password in domain error

--- src/app/visitor/login/domain/invalid-password.error.ts
+++ src/app/visitor/login/domain/invalid-password.error.ts
@@ -1,6 +1,6 @@
 // Gestion de l'erreur 'Mot de passe invalide' et du message correspondant
 export class InvalidPasswordError extends Error {
-  constructor() {
+  constructor(readonly password: string) {
     super(`Invalid password. Please try another password.`);
     this.name = 'InvalidPasswordError';
   }
