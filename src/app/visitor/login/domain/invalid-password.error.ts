2ac2e3ea797ea958d20396fc9ab85de1a0369dd2
Xavier DAVID <xav.david28@gmail.com>
2025-04-19 12:54:55 +0200

    wip(login): inject use case + bind submit event

--- src/app/visitor/login/domain/invalid-password.error.ts
+++ src/app/visitor/login/domain/invalid-password.error.ts
@@ -1,5 +1,5 @@
 // Gestion de l'erreur 'Mot de passe invalide' et du message correspondant
-export class InvaliPasswordError extends Error {
+export class InvalidPasswordError extends Error {
   constructor() {
     super(`Invalid password. Please try another password.`);
     this.name = 'InvalidPasswordError';
--- src/app/visitor/login/login.page.component.ts
+++ src/app/visitor/login/login.page.component.ts
@@ -1,5 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { LoginUserUseCase } from './domain/login-user.use-case';
+import { UserEmailNotFoundError } from './domain/user-email-not-found-error';
+import { InvalidPasswordError } from './domain/invalid-password.error';
 
 @Component({
   imports: [FormsModule],
@@ -7,7 +10,20 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.page.component.scss'
 })
 export class LoginPageComponent {
+  readonly #loginUserUseCase = inject(LoginUserUseCase)
   readonly email = signal('');
   readonly password = signal('');
 
+  // Gestion de la soumission du formulaire de connexion
+  onSubmit() {
+    this.#loginUserUseCase.execute(this.email(), this.password()).catch(error => {
+      if(error instanceof UserEmailNotFoundError) {
+        this.userEmailNotFoundError.set(error);
+      }
+
+      if(error instanceof InvalidPasswordError) {
+        this.invalidPasswordError.set(error);
+      } 
+    })
+  }
 }
