b894ac06b8c02a1b3a48c4f1c1b13dd3276e2d49
Xavier DAVID <xav.david28@gmail.com>
2025-04-19 13:10:47 +0200

    wip(login): implement login use case

--- src/app/visitor/login/domain/login-user.use-case.ts
+++ src/app/visitor/login/domain/login-user.use-case.ts
@@ -1,9 +1,48 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '@app/core/port/authentication.service';
+import { UserService } from '@app/core/port/user.service';
+import { UserStore } from '@app/core/store/user.store';
+import { firstValueFrom } from 'rxjs';
+import { InvalidPasswordError } from './invalid-password.error';
+import { UserEmailNotFoundError } from './user-email-not-found-error';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginUserUseCase {
+  readonly #authenticationService = inject(AuthenticationService);
+  readonly #userService = inject(UserService);
+  readonly #userStore = inject(UserStore);
+  readonly #router = inject(Router);
 
-  
+  async execute(email: string, password: string): Promise<void> {
+    // 1. Authenticate existing user
+    const response = await firstValueFrom(this.#authenticationService.login(email, password));
+
+    // 2. Throw an errof if email does not exist
+    if(response instanceof UserEmailNotFoundError) {
+      throw response;
+    }
+
+    // 3. Throw an errof if password is invalid
+    if(response instanceof InvalidPasswordError) {
+      throw response;
+    }
+
+    // 4. Add credentials information in webapp storage
+    const { jwtToken, jwtRefreshToken, expiresIn, userId } = response;
+
+    localStorage.setItem('jwtToken', jwtToken);
+    localStorage.setItem('jwtRefreshToken', jwtRefreshToken);
+    localStorage.setItem('expiresIn', expiresIn);
+
+
+    // 5. Add user in app store
+    const user = await firstValueFrom(this.#userService.fetch(userId));
+    this.#userStore.login(user);
+
+    // 6. Redirect user to dashboard
+    this.#router.navigate(['/app/dashboard']);
+  }
 }
