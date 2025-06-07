a98ba7575302db325b7c0a810b3032fb0fead095
Xavier DAVID <xav.david28@gmail.com>
2025-06-07 10:24:10 +0200

    wip(membership): generate settings page component

---
+++ src/app/membership/settings/settings.page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './settings.page.component.html',
+  styleUrl: './settings.page.component.scss'
+})
+export class SettingsPageComponent {
+
+}
