4c0c52456ad1879a0717d80a16016d7c06dfb3b0
Xavier DAVID <xav.david28@gmail.com>
2025-06-07 11:20:38 +0200

    feat(webapp): implement lazy loading strategy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,52 +1,44 @@
 import { Routes } from '@angular/router';
-import { HomePageComponent } from './visitor/home/home.page.component';
-import { SignupPageComponent } from './visitor/signup/signup.page.component';
-import { LoginPageComponent } from './visitor/login/login.page.component';
-import { DashboardPageComponent } from './membership/dashboard/dashboard.page.component';
-import { WorkdayPageComponent } from './membership/workday/workday.page.component';
-import { PlanningPageComponent } from './membership/planning/planning.page.component';
-import { ProfilePageComponent } from './membership/profile/profile.page.component';
-import { SettingsPageComponent } from './membership/settings/settings.page.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomePageComponent,
-    title: 'Productivity Planner'
+    loadComponent: () => import('./visitor/home/home.page.component').then(c => c.HomePageComponent),
+    title: 'Productivity Planner',
   },
   {
     path: 'signup',
-    component: SignupPageComponent,
-    title: 'Signup'
+    loadComponent: () => import('./visitor/signup/signup.page.component').then(c => c.SignupPageComponent),
+    title: 'Signup',
   },
   {
     path: 'login',
-    component: LoginPageComponent,
-    title: 'Login'
+    loadComponent: () => import('./visitor/login/login.page.component').then(c => c.LoginPageComponent),
+    title: 'Login',
   },
   {
     path: 'app/dashboard',
-    component: DashboardPageComponent,
-    title: 'Dashboard'
+    loadComponent: () => import('./membership/dashboard/dashboard.page.component').then(c => c.DashboardPageComponent),
+    title: 'Dashboard',
   },
   {
     path: 'app/planning',
-    component: PlanningPageComponent,
-    title: 'Planning'
+    loadComponent: () => import('./membership/planning/planning.page.component').then(c => c.PlanningPageComponent),
+    title: 'Planning',
   },
   {
     path: 'app/workday',
-    component: WorkdayPageComponent,
-    title: 'Workday'
+    loadComponent: () => import('./membership/workday/workday.page.component').then(c => c.WorkdayPageComponent),
+    title: 'Workday',
   },
   {
     path: 'app/profile',
-    component: ProfilePageComponent,
-    title: 'Profile'
+    loadComponent: () => import('./membership/profile/profile.page.component').then(c => c.ProfilePageComponent),
+    title: 'Profile',
   },
   {
     path: 'app/settings',
-    component: SettingsPageComponent,
-    title: 'Settings'
+    loadComponent: () => import('./membership/settings/settings.page.component').then(c => c.SettingsPageComponent),
+    title: 'Settings',
   },
 ];
