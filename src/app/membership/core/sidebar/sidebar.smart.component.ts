5b88721d53450785ad8bf2e15aefec28c2a11181
Xavier DAVID <xav.david28@gmail.com>
2025-06-14 12:40:09 +0200

    wip(sidebar): add redirection link

--- src/app/membership/core/sidebar/sidebar.smart.component.ts
+++ src/app/membership/core/sidebar/sidebar.smart.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebar.smart.component.html',
   styleUrl: './sidebar.smart.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
