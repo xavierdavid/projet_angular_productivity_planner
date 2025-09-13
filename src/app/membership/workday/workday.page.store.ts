40c3a8817fe6078a55cb1f0aa51f998e068d1571
Xavier DAVID <xav.david28@gmail.com>
2025-09-13 12:28:02 +0200

    feat(workday): setup add task

--- src/app/membership/workday/workday.page.store.ts
+++ src/app/membership/workday/workday.page.store.ts
@@ -1,19 +1,17 @@
 import { computed } from '@angular/core';
-import { signalStore, withComputed, withState } from '@ngrx/signals';
+import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
 
 // Contrat de structure de données pour l'objet 'Pomodoro'
 interface Pomodoro {
   status: 'Not started' | 'In progress' | 'Done';
+  currentTime: number;
+  duration: number;
+  isCompleted: boolean;
 }
 
-// Typage de la variable 'PomodoroList' (tableau contenant 0 à 5 pomodoros au maximum)
-type PomodoroList =
-  | [Pomodoro]
-  | [Pomodoro, Pomodoro]
-  | [Pomodoro, Pomodoro, Pomodoro]
-  | [Pomodoro, Pomodoro, Pomodoro, Pomodoro]
-  | [Pomodoro, Pomodoro, Pomodoro, Pomodoro, Pomodoro];
-
+// Typage de la variable 'PomodoroList'
+type PomodoroList = Pomodoro[];
+  
 // Contrat de structure de données pour l'objet 'Task'
 interface Task {
   type: 'Hit the target' | 'Get things done';
@@ -22,32 +20,34 @@ interface Task {
   pomodoroList: PomodoroList;
 }
 
-// Typage de la variable 'liste de tâches' (tableau contenant 1 à 6 tâches au maximum)
-type TaskList =
-  | [Task]
-  | [Task, Task]
-  | [Task, Task, Task]
-  | [Task, Task, Task, Task]
-  | [Task, Task, Task, Task, Task]
-  | [Task, Task, Task, Task, Task, Task];
-
+// Typage de la variable 'liste de tâches' 
+type TaskList = Task[]
+  
 // Contrat de structure de données pour l'objet 'WorkdayState'
 interface WorkdayState {
   date: string;
   taskList: TaskList;
 };
 
-// Description d'une constante au démarrage (valeurs par défaut)
-const initialState: WorkdayState = {
-  date: '',
-  taskList: [
+// Description d'une constante pour une nouvelle tâche
+const getEmptyTask = (): Task => ({
+  type: 'Hit the target',
+  title: 'Nouvelle tâche',
+  pomodoroCount: 1,
+  pomodoroList: [
     {
-      type: 'Hit the target',
-      title: 'Nouvelle tâche',
-      pomodoroCount: 1,
-      pomodoroList: [{ status: 'Not started' }],
+      status: 'Not started',
+      currentTime: 0,
+      duration: 1500,
+      isCompleted: false, 
     },
   ],
+});
+
+// Description d'une constante au démarrage (valeurs par défaut de l'Etat initial)
+const initialState: WorkdayState = {
+  date: '',
+  taskList: [getEmptyTask()],
 };
 
 // Démarrage du Store
@@ -60,4 +60,12 @@ export const WorkdayStore = signalStore(
 
     return { mostImportantTask };
   }),
+  // Gestion de l'interaction avec le bouton 'ajouter une tâche' 
+  withMethods((store) => ({
+    // Réponse à l'évènement clic 'ajouter une tâche'
+    onAddTask() {
+      // On patch la nouvelle tâche dans le state (modification du state de manière immutable)
+      patchState(store, (state) => ({ taskList: [...state.taskList, getEmptyTask()] }))
+    }
+  }))
 );
